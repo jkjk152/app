94956ae4c12c4f4b9b5650c59bbe1b9e1ccd14b6
Mark Scherer <dereuromark@users.noreply.github.com>
2024-07-25 11:54:24 +0200

    Remove not needed cloaking check

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -155,7 +155,7 @@ if (!$fullBaseUrl) {
     }
 
     $httpHost = env('HTTP_HOST');
-    if (isset($httpHost)) {
+    if ($httpHost !== null) {
         $fullBaseUrl = 'http' . $s . '://' . $httpHost;
     }
     unset($httpHost, $s);
